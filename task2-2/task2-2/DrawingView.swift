b042a4d586455e42ce5a152a093f687fdb869653
Alex <81694529+Clepfeed@users.noreply.github.com>
2025-05-26 00:34:21 +0300

    feature-task2-3

--- task2-2/task2-2/DrawingView.swift
+++ task2-2/task2-2/DrawingView.swift
@@ -8,22 +8,60 @@
 	
 import UIKit
 
+enum AnimationType: Int {
+    case move
+    case rotate
+    case scale
+    case fade
+    case moveAndRotate
+}
+
 class DrawingView: UIView {
 
     var shapeType: ShapeType = .triangle {
-        didSet { setNeedsDisplay() }
+        didSet { redraw() }
     }
 
     var fillStyle: FillStyle = .solid {
-        didSet { setNeedsDisplay() }
+        didSet { redraw() }
     }
 
-    override func draw(_ rect: CGRect) {
-        guard let context = UIGraphicsGetCurrentContext() else { return }
+    private let shapeLayer = CAShapeLayer()
 
-        let insetRect = rect.insetBy(dx: 20, dy: 20)
-        let triangle = UIBezierPath.curvedTriangle(in: insetRect)
-        let hexagon = UIBezierPath.hexagon(in: insetRect)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    private func setup() {
+        layer.addSublayer(shapeLayer)
+        shapeLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        redraw()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        redraw()
+    }
+
+    private func redraw() {
+        let boundsRect = bounds.insetBy(dx: 40, dy: 40)
+
+        // Уменьшенный треугольник
+        let shapeRect: CGRect
+        if shapeType != .hexagon {
+            shapeRect = boundsRect.insetBy(dx: boundsRect.width * 0.25, dy: boundsRect.height * 0.25)
+        } else {
+            shapeRect = boundsRect
+        }
+
+        let triangle = UIBezierPath.curvedTriangle(in: shapeRect)
+        let hexagon = UIBezierPath.hexagon(in: shapeRect)
 
         var path: UIBezierPath
 
@@ -33,34 +71,93 @@ class DrawingView: UIView {
         case .hexagon:
             path = hexagon
         case .union:
-            path = .unionPath(triangle, hexagon)
+            path = UIBezierPath()
+            path.append(triangle)
+            path.append(hexagon)
+            path.usesEvenOddFillRule = false
         case .subtract:
-            path = .subtractPath(triangle, hexagon)
+            path = UIBezierPath()
+            path.append(triangle)
+            path.append(hexagon)
+            path.usesEvenOddFillRule = true
         }
+
+        shapeLayer.path = path.cgPath
+        shapeLayer.fillRule = path.usesEvenOddFillRule ? .evenOdd : .nonZero
+
+        applyFillStyle()
+    }
+
+    private func applyFillStyle() {
+        shapeLayer.sublayers?.forEach { $0.removeFromSuperlayer() }
+        shapeLayer.shadowOpacity = 0
+        shapeLayer.opacity = 1.0
+        shapeLayer.transform = CATransform3DIdentity
 
         switch fillStyle {
         case .solid:
-            UIColor.systemBlue.setFill()
-            path.fill()
+            shapeLayer.fillColor = UIColor.systemBlue.cgColor
         case .gradient:
-            context.saveGState()
-            path.addClip()
-            let colors = [UIColor.systemPurple.cgColor, UIColor.systemTeal.cgColor] as CFArray
-            let space = CGColorSpaceCreateDeviceRGB()
-            if let gradient = CGGradient(colorsSpace: space, colors: colors, locations: [0, 1]) {
-                context.drawLinearGradient(gradient,
-                                           start: CGPoint(x: rect.minX, y: rect.minY),
-                                           end: CGPoint(x: rect.maxX, y: rect.maxY),
-                                           options: [])
-            }
-            context.restoreGState()
+            let gradient = CAGradientLayer()
+            gradient.frame = bounds
+            gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemTeal.cgColor]
+            gradient.mask = shapeLayer
+            layer.addSublayer(gradient)
         case .shadow:
-            context.saveGState()
-            context.setShadow(offset: CGSize(width: 5, height: 5), blur: 10, color: UIColor.black.withAlphaComponent(0.5).cgColor)
-            UIColor.systemGreen.setFill()
-            path.fill()
-            context.restoreGState()
+            shapeLayer.fillColor = UIColor.systemGreen.cgColor
+            shapeLayer.shadowColor = UIColor.black.cgColor
+            shapeLayer.shadowOffset = CGSize(width: 5, height: 5)
+            shapeLayer.shadowRadius = 8
+            shapeLayer.shadowOpacity = 0.5
+        }
+    }
+
+    // MARK: - Анимации
+
+    func applyAnimation(type: AnimationType) {
+        shapeLayer.removeAllAnimations()
+
+        switch type {
+        case .move:
+            let anim = CABasicAnimation(keyPath: "position")
+            anim.fromValue = shapeLayer.position
+            anim.toValue = CGPoint(x: shapeLayer.position.x + 80, y: shapeLayer.position.y)
+            anim.duration = 1.0
+            anim.autoreverses = true
+            shapeLayer.add(anim, forKey: "position")
+        case .rotate:
+            let anim = CABasicAnimation(keyPath: "transform.rotation")
+            anim.toValue = CGFloat.pi
+            anim.duration = 1.0
+            anim.autoreverses = true
+            shapeLayer.add(anim, forKey: "rotation")
+        case .scale:
+            let anim = CABasicAnimation(keyPath: "transform.scale")
+            anim.fromValue = 1.0
+            anim.toValue = 1.5
+            anim.duration = 1.0
+            anim.autoreverses = true
+            shapeLayer.add(anim, forKey: "scale")
+        case .fade:
+            let anim = CABasicAnimation(keyPath: "opacity")
+            anim.fromValue = 1.0
+            anim.toValue = 0.2
+            anim.duration = 1.0
+            anim.autoreverses = true
+            shapeLayer.add(anim, forKey: "opacity")
+        case .moveAndRotate:
+            let move = CABasicAnimation(keyPath: "position")
+            move.toValue = CGPoint(x: shapeLayer.position.x + 60, y: shapeLayer.position.y + 40)
+
+            let rotate = CABasicAnimation(keyPath: "transform.rotation")
+            rotate.toValue = CGFloat.pi
+
+            let group = CAAnimationGroup()
+            group.animations = [move, rotate]
+            group.duration = 1.2
+            group.autoreverses = true
+
+            shapeLayer.add(group, forKey: "moveAndRotate")
         }
     }
 }
-
--- task2-2/task2-2/ViewController.swift
+++ task2-2/task2-2/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet var drawingView: DrawingView!
     
     
+    @IBOutlet weak var animationSelector: UISegmentedControl!
     @IBAction func shapeChanged(_ sender: UISegmentedControl) {
         if let selected = ShapeType(rawValue: sender.selectedSegmentIndex) {
                     drawingView.shapeType = selected
@@ -22,6 +23,13 @@ class ViewController: UIViewController {
         if let selected = FillStyle(rawValue: sender.selectedSegmentIndex) {
                     drawingView.fillStyle = selected
                 }
+    }
+    
+    
+    @IBAction func applyAnimation(_ sender: Any) {
+        if let selected = AnimationType(rawValue: animationSelector.selectedSegmentIndex) {
+            drawingView.applyAnimation(type: selected)
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
